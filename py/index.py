eb5ed79ccfe7d0eacaad13275b06bfa661228f13
YinYong <yanni4night@gmail.com>
2014-11-22 13:40:03 +0800

    use stdin&stdout to avoid long shell parameter

--- py/index.py
+++ py/index.py
@@ -2,7 +2,7 @@
 
 from django.template import Context, loader
 from django.conf import settings
-from urllib import quote, unquote
+from urllib import unquote
 import django, sys, json
 
 
@@ -11,32 +11,30 @@ class Session(object):
     def __init__(self, template_dirs = '.'):
         super(Session, self).__init__()
         self.template_dirs = template_dirs
-        settings.configure(DEBUG = True, TEMPLATE_DEBUG = True, TEMPLATE_DIRS = [template_dirs])
+        settings.configure(DEBUG = True, TEMPLATE_DEBUG = True, TEMPLATE_DIRS = [template_dirs], LANGUAGE_CODE = 'zh-cn', FILE_CHARSET = 'utf-8', TIME_ZONE = 'Asia/Shanghai')
         django.setup()
     def render(self, tpl, data = {}):
+        """Render a template with mock data"""
         template = loader.get_template(tpl)
         context = Context(data)
         return template.render(context)
     def __str__(self):
-        print 'template_dirs:' + template_dirs
-
-#not used
-SessionCache = {}
+        print 'template_dirs:' + self.template_dirs
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        raise Exception('"template" and "data" must be indicated')
+        raise Exception('"template_dirs" and "template" BOTH are required')
     else:
-        dirs = unquote(sys.argv[1])
-        session = SessionCache.get(dirs)
-        if not session:
-            session = Session(dirs)
-            SessionCache[dirs] = session
-        tpl = unquote(sys.argv[2])
+        dirs = unquote(sys.argv[1])#template_dirs is a directory path that are quoted
+        session = Session(dirs)
+
+        tpl = unquote(sys.argv[2])#template is a file path that are quoted
+
+        mockStr = sys.stdin.readline();#We read the mock data from stdin
+
         try:
-            data = json.loads(unquote(sys.argv[3]))
+            data = json.loads(mockStr)
         except Exception,e:
             data = {}
-        html = session.render(tpl, data)
-        print quote(html.encode('utf-8'))
 
+        print session.render(tpl, data).encode('utf-8')
